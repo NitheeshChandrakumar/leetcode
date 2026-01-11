262ba44f0bcc7d3f94a4f93953e958d7a4817816
Nitheesh C <nitheeshchandrakumar11@gmail.com>
2026-01-11 21:05:15 +0530

    Time: 3 ms (35.45%) | Memory: 12.9 MB (7.54%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.c
+++ 2-add-two-numbers/add-two-numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 //declare variables
+
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
     struct ListNode dummy;
     struct ListNode* tail = &dummy;
