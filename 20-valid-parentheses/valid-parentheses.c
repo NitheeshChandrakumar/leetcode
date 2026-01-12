e4c6083d36ad5a2e77b750d3bf754b81d9b86074
Nitheesh C <nitheeshchandrakumar11@gmail.com>
2026-01-12 20:41:30 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (74.34%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool isValid(char *s) {
+    int len = strlen(s);
+    char stack[len];
+    int top = -1;
+
+    for (int i = 0; i < len; i++) {
+        char c = s[i];
+
+        if (c == '(' || c == '{' || c == '[') {
+            stack[++top] = c;
+        }
+    
+        else {
+            if (top == -1)
+                return false;
+
+            char open = stack[top--];
+
+            if ((c == ')' && open != '(') ||
+                (c == '}' && open != '{') ||
+                (c == ']' && open != '[')) {
+                return false;
+            }
+        }
+    }
+
+ 
+    return top == -1;
+}
