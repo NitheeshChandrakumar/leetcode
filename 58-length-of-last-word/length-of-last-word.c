ccce522a056a1b828443d49f866b03947e08709c
Nitheesh C <nitheeshchandrakumar11@gmail.com>
2026-01-17 16:19:09 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (32.64%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int lengthOfLastWord(char* s) {
+    int i = 0;
+    int length = 0;
+    while (s[i] != '\0')
+    i++;
+    i--;
+    while (i >= 0 && s[i] == ' ')
+        i--;
+    while (i >= 0 && s[i] != ' ') {
+        length++;
+        i--;
+    }
+
+    return length;
+}
